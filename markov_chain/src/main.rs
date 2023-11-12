7a27c3521412d95d437358d67e2f1cf30d511f67
simon0820s <simon0820s@gmail.com>
2023-11-11 23:47:50 -0500

    traduction

--- markov_chain/src/main.rs
+++ markov_chain/src/main.rs
@@ -1,11 +1,11 @@
 const INITIAL_STATE: [u16; 3] = [2000, 40000, 18000];
-const TRANSITION_MATRIZ: [[f32; 3]; 3] = [
+const TRANSITION_MATRIX: [[f32; 3]; 3] = [
     [0.8, 0.2, 0.05],
     [0.05, 0.75, 0.05],
     [0.15, 0.05, 0.9]];
 fn main() {
     print_initial_state();
-    
+
     let state: [f32; 3] = state_by_time(25);
 
     print_state(state)
@@ -30,7 +30,7 @@ fn print_initial_state() {
 
     println!("===Transition Matriz===");
 
-    for m_item in TRANSITION_MATRIZ {
+    for m_item in TRANSITION_MATRIX {
         for item in m_item {
             print!(" {}", item)
         }
@@ -45,7 +45,7 @@ fn state_by_time (time: u8) -> [f32; 3] {
 
     let mut i: f32 = 0.0;
 
-    for (m_index, m_item) in TRANSITION_MATRIZ.iter().enumerate() {
+    for (m_index, m_item) in TRANSITION_MATRIX.iter().enumerate() {
         for (index, item) in m_item.iter().enumerate() {
             i += item * f32::from(INITIAL_STATE[index])
         }
