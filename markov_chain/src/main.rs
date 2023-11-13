3f6a25cb5bd4d38d88370ee24c8157089a35880e
simon0820s <simon0820s@gmail.com>
2023-11-13 12:06:20 -0500

    two iteration

--- markov_chain/src/main.rs
+++ markov_chain/src/main.rs
@@ -1,4 +1,4 @@
-const INITIAL_STATE: [u16; 3] = [2000, 40000, 18000];
+const INITIAL_STATE: [f32; 3] = [2000.0, 40000.0, 18000.0];
 const TRANSITION_MATRIX: [[f32; 3]; 3] = [
     [0.8, 0.2, 0.05],
     [0.05, 0.75, 0.05],
@@ -7,7 +7,7 @@ const TRANSITION_MATRIX: [[f32; 3]; 3] = [
 fn main() {
     print_initial_state();
 
-    let state: [f32; 3] = state_by_time(25);
+    let state: [f32; 3] = state_by_time(state_by_time(INITIAL_STATE));
 
     print_state(state)
 }
@@ -20,7 +20,8 @@ fn print_state(state: [f32; 3]) {
 }
 
 fn print_initial_state() {
-    println!("====initial state====");
+    println!();
+    println!("🦀====initial state====🦀");
     println!("");
 
     for (index, item) in INITIAL_STATE.iter().enumerate() {
@@ -29,7 +30,7 @@ fn print_initial_state() {
 
     println!();
 
-    println!("===Transition Matriz===");
+    println!("🦀===Transition Matriz===🦀");
 
     for m_item in TRANSITION_MATRIX {
         for item in m_item {
@@ -37,9 +38,10 @@ fn print_initial_state() {
         }
         println!() 
     }
+    println!();
 }
 
-fn state_by_time (time: u8) -> [f32; 3] {
+fn state_by_time (previous_state: [f32; 3]) -> [f32; 3] {
     // x_k+1 = Px_k
 
     let mut state = [0.0; 3];
@@ -48,11 +50,14 @@ fn state_by_time (time: u8) -> [f32; 3] {
 
     for (m_index, m_item) in TRANSITION_MATRIX.iter().enumerate() {
         for (index, item) in m_item.iter().enumerate() {
-            i += item * f32::from(INITIAL_STATE[index])
+            i += item * f32::from(previous_state[index])
         }
         state[m_index] += i;
         i = 0.0
     }
 
-    return state;
+    print_state(state);
+    println!();
+    println!();
+    state
 }
